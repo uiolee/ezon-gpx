861276222418c30d4877e937959cea18bc189b1d
uiolee <22849383+uiolee@users.noreply.github.com>
2025-01-22 10:58:41 +0800

    test: output version
    [skip ci]

--- test/cli.test.sh
+++ test/cli.test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python --version
-pipx --version
+echo "python: " $(python --version)
+echo "pipx  : " $(pipx --version)
 pipx run --python python"$python_version" --spec . ezon-gpx test
